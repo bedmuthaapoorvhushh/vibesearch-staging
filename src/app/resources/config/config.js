7f7253ac9eeeea8b36d8a87002f9ac919ee7d97b
Apoorv Bedmutha <i-apoorv@hushh.ai>
2024-11-04 18:15:06 +0530

    Added staging configs

--- src/app/resources/config/config.js
+++ src/app/resources/config/config.js
@@ -1,25 +1,37 @@
 import { createClient } from "@supabase/supabase-js";
 import brands from "./brands";
 import queries from "./queries";
-import genders from './genders'
+import genders from "./genders";
+let redirect_urls = {
+  production: "https://vibesearch.ai",
+  development: "http://localhost:3000",
+  staging: "https://vibesearch-staging.vercel.app",
+};
+let vibesearchAPIEndpoints = {
+  production: "https://vibe-search-service-53407187172.us-east4.run.app",
+  development:
+    "https://vibe-search-service-staging-53407187172.us-east4.run.app",
+  staging: "https://vibe-search-service-staging-53407187172.us-east4.run.app",
+};
+let monitoringEndpoints = {
+  production: "https://vibe-search-service-53407187172.us-east4.run.app",
+  development:
+    "https://vibe-profile-service-staging-53407187172.us-east4.run.app",
+  staging: "https://vibe-profile-service-staging-53407187172.us-east4.run.app",
+};
 const config = {
   SUPABASE_URL: "https://eaeokyefsdfamwqqzfko.supabase.co",
   SUPABASE_ANON_KEY:
     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVhZW9reWVmc2RmYW13cXF6ZmtvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDkzNzExODYsImV4cCI6MjAyNDk0NzE4Nn0.iqWMB0debYgPRX5PjtJuIfy5ImxZfC9ol7A4EPFXmFU",
   supabaseClient: null,
-  vibesearchAPIEndpoint:
-    "https://vibe-search-service-53407187172.us-east4.run.app",
-  monitoringEndpoint:
-    "https://vibe-search-version3-53407187172.us-central1.run.app",
+  vibesearchAPIEndpoint: vibesearchAPIEndpoints[process.env.NODE_ENV],
+  monitoringEndpoint: monitoringEndpoints[process.env.NODE_ENV],
   guestModeAccessToken:
     "P2H8RNXPvIiPoeM0iJEDjJ2Skk37h5pScMQF5oMRUXm3dKoUC2wxrWImx5ccA9VOrOoeaLcMQqn57vYDPucTkYnkkH6icUQy09vtd5eIrAIXhBtmUfAmPI3thD2OoUeF",
-  redirect_url:
-    process.env.NODE_ENV === "production"
-      ? "https://vibesearch.ai"
-      : "http://localhost:3000",
+  redirect_url: redirect_urls[process.env.NODE_ENV],
   brands: brands,
   queries: queries,
-  genders: genders
+  genders: genders,
 };
 
 function createSupabaseClient() {
