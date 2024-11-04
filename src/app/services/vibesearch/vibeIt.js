9c59f05c06fd1f55c2308eead3052236efcb3a02
Apoorv Bedmutha <i-apoorv@hushh.ai>
2024-11-04 18:51:20 +0530

    initial changes

--- src/app/services/vibesearch/vibeIt.js
+++ src/app/services/vibesearch/vibeIt.js
@@ -18,7 +18,7 @@ export default async function vibeIt(
   price_range
 ) {
   if (mainQuery == "" && secondaryQuery == "") return;
-  console.log(process.env.NODE_ENV)
+  console.log(process.env.SITE_ENV)
   if (!access_token && process.env.SITE_ENV !== "staging")
     window.location.href = config.redirect_url + "/components/ErrorNoLogin";
   let data = {
